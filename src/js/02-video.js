f450603b6899572094f0f9dba56da2a5b6d0519e
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-21 22:44:23 +0300

    task2+

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,34 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 
+const STORAGE_KEY = 'videoplayer-current-time'
+const timeCode = Number(localStorage.getItem(STORAGE_KEY));
+
+
+
 const player = new Player(iframe);
-console.log(player);
 
-player.on('play', function() {
-        console.log('played the video!');
-    });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
 
 
 const onPlay = function(data) {
-    console.log(data)
+    localStorage.setItem(STORAGE_KEY, data.seconds);
 };
 
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(timeCode).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
