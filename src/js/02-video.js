862e12dba601a58beec5f60e6234c27cd67ba5b8
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-21 22:03:34 +0300

    ..

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,20 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 
 const player = new Player(iframe);
+console.log(player);
 
 player.on('play', function() {
         console.log('played the video!');
     });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+
+const onPlay = function(data) {
+    console.log(data)
+};
+
+
+player.on('timeupdate', onPlay);
