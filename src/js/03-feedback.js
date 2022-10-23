088664e6b84c85374699ed14ea05e191163d3571
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-23 17:17:22 +0300

    ...

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,30 +2,31 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-from-state';
 
-const formData = {};
+
 
 const refs = {
     form: document.querySelector('.feedback-form'),
-    textArea: document.querySelector('.feedback-form textarea'),
-    emailInput: document.querySelector('.feedback-form input'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
+function getLsData() {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+};
+
 function onFormInput(e) {
+    const formData = getLsData();
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 };
 
 
 function fillForm() {
-    const savedFormData = localStorage.getItem(STORAGE_KEY);
-    const parsedFormData = JSON.parse(savedFormData);
+    const formData = getLsData();
 
-    if (savedFormData) {
-        refs.emailInput.value = parsedFormData.email;
-        refs.textArea.value = parsedFormData.message;
+    for (let key in formData) {
+        refs.form.elements[key].value = formData[key];
     }
 }
 
@@ -36,10 +37,7 @@ function onFormSubmit(e) {
 
     e.currentTarget.reset();
 
-    const savedFormData = localStorage.getItem(STORAGE_KEY);
-    const parsedFormData = JSON.parse(savedFormData);
-    
-    console.log(parsedFormData);
+    console.log(getLsData())
 
     localStorage.removeItem(STORAGE_KEY);
 };
